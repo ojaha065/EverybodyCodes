99f0febe95571b356d0d581f7a4c0733997923e6
Jani Haiko <haiko.jani@gmail.com>
2025-10-12 00:13:30 +0300

    Quest 3

---
+++ src/main/java/fi/kissakala/TheKingdomOfAlgorithmia.java
@@ -0,0 +1,168 @@
+package fi.kissakala;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import static fi.kissakala.Utils.*;
+
+/**
+ * <a href="https://everybody.codes/event/2024/quests">The Kingdom of Algorithmia</a>
+ */
+public class TheKingdomOfAlgorithmia {
+	private static final Pattern QUEST_2_PATTERN = Pattern.compile("^WORDS:([A-Z,]+)[\\r\\n]+([A-Z\\s,.]+)$");
+
+	static void main() throws Exception {
+		testAll();
+
+		IO.println("=== Quest 1 ===");
+		run("Part 1", () -> calculatePotionsForEnemies(readInputAsCharArray("Quest1Part1.txt")));
+		run("Part 2", () -> calculatePotionsForGroups(readInput("Quest1Part2.txt"), 2));
+		run("Part 3", () -> calculatePotionsForGroups(readInput("Quest1Part3.txt"), 3));
+
+		IO.println("=== Quest 2 ===");
+		run("Part 1", () -> countRunicWordsAndSymbols(readInput("Quest2Part1.txt"), false).wordCount());
+		run("Part 2", () -> countRunicWordsAndSymbols(readInput("Quest2Part2.txt"), true).symbolsCount());
+
+		IO.println("=== Quest 3 ===");
+		run("Part 1", () -> slopeCalculator(readInput("Quest3Part1.txt"), false));
+		run("Part 2", () -> slopeCalculator(readInput("Quest3Part2.txt"), false));
+		run("Part 3", () -> slopeCalculator(readInput("Quest3Part3.txt"), true));
+	}
+
+	private static int calculatePotionsForEnemies(final char... input) {
+		int result = 0;
+		for (final char c : input) {
+			result += switch (c) {
+				case 'B' -> 1;
+				case 'C' -> 3;
+				case 'D' -> 5; // Parts 2 and 3 only
+				default -> 0;
+			};
+		}
+		return result;
+	}
+	private static int calculatePotionsForGroups(final String input, final int groupSize) {
+		final List<Character> currentGroup = new ArrayList<>();
+		int result = 0;
+
+		for (final char c : input.toCharArray()) {
+			currentGroup.add(c);
+			if (currentGroup.size() == groupSize) {
+				result += currentGroup.stream().mapToInt(TheKingdomOfAlgorithmia::calculatePotionsForEnemies).sum();
+
+				final long enemyCount = currentGroup.stream().filter(thisC -> thisC.equals('A') || thisC.equals('B') || thisC.equals('C') || thisC.equals('D')).count();
+				if (enemyCount == 3L) {
+					result += 6;
+				}
+				else if (enemyCount == 2L) {
+					result += 2;
+				}
+
+				currentGroup.clear();
+			}
+		}
+
+		return result;
+	}
+
+	private static RunicWordsAndSymbolsCount countRunicWordsAndSymbols(final String input, final boolean doReverseSearch) {
+		final Matcher matcher = matchInput(input, QUEST_2_PATTERN);
+		final char[][] grid = Arrays.stream(LINE_BREAK_PATTERN.split(matcher.group(2)))
+			.map(String::toCharArray)
+			.toArray(char[][]::new);
+
+		final List<String> words = new ArrayList<>(Arrays.asList(matcher.group(1).split(",")));
+		if (doReverseSearch) {
+			words.addAll(words.stream().map(Utils::reverse).toList());
+		}
+
+		int wordCount = 0;
+		for (int y = 0; y < grid.length; y++) {
+			final int finalY = y;
+			final String inscription = new String(grid[y]);
+
+			for (final String word : words) {
+				int index = 0;
+				while ((index = inscription.indexOf(word, index)) != -1) {
+					wordCount++;
+					IntStream.range(index, index + word.length()).forEach(i -> grid[finalY][i] = Character.toLowerCase(grid[finalY][i]));
+					index++;
+				}
+			}
+		}
+
+		return new RunicWordsAndSymbolsCount(
+			wordCount,
+			Arrays.stream(grid)
+				.mapToLong(row -> IntStream.range(0, row.length).filter(i -> Character.isLowerCase(row[i])).count())
+				.sum()
+		);
+	}
+
+	private static int slopeCalculator(final String input, final boolean diagonal) {
+		final Integer[][] grid = pad2DArray(stringAs2DArray(input, s -> s.equals("#") ? 1 : 0, Integer.class), 1, 0);
+
+		final List<XY> toBeDug = new ArrayList<>();
+		do {
+			toBeDug.clear();
+
+			for (int y = 0; y < grid.length; y++) {
+				for (int x = 0; x < grid[y].length; x++) {
+					if (grid[y][x] == 0) continue;
+
+					if (Objects.equals(grid[y][x - 1], grid[y][x]) // left
+						&& Objects.equals(grid[y - 1][x], grid[y][x]) // up
+						&& Objects.equals(grid[y][x + 1], grid[y][x]) // right
+						&& Objects.equals(grid[y + 1][x], grid[y][x]) // down
+						&& (!diagonal || Objects.equals(grid[y - 1][x - 1], grid[y][x]))
+						&& (!diagonal || Objects.equals(grid[y - 1][x + 1], grid[y][x]))
+						&& (!diagonal || Objects.equals(grid[y + 1][x - 1], grid[y][x]))
+						&& (!diagonal || Objects.equals(grid[y + 1][x + 1], grid[y][x]))
+					) { toBeDug.add(new XY(x, y)); }
+				}
+			}
+
+			toBeDug.forEach(xy -> grid[xy.y()][xy.x()]++);
+		} while (!toBeDug.isEmpty());
+
+		return Arrays.stream(grid)
+			.mapToInt(row -> Arrays.stream(row).reduce(0, Integer::sum))
+			.sum();
+	}
+
+	private static void testAll() {
+		expect(calculatePotionsForEnemies("ABBAC".toCharArray()), 5);
+		expect(calculatePotionsForGroups("AxBCDDCAxD", 2), 28);
+		expect(calculatePotionsForGroups("xBxAAABCDxCC", 3), 30);
+
+		Map.of(
+			"AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE", 4,
+			"THE FLAME SHIELDED THE HEART OF THE KINGS", 3,
+			"POWE PO WER P OWE R", 2,
+			"THERE IS THE END", 3
+		).forEach((inscription, count) -> expect(countRunicWordsAndSymbols("WORDS:THE,OWE,MES,ROD,HER\n\n" + inscription, false).wordCount(), count));
+		expect(
+			countRunicWordsAndSymbols("WORDS:THE,OWE,MES,ROD,HER,QAQ\n\n" + String.join("\n", List.of(
+				"AWAKEN THE POWE ADORNED WITH THE FLAMES BRIGHT IRE",
+				"THE FLAME SHIELDED THE HEART OF THE KINGS",
+				"POWE PO WER P OWE R",
+				"THERE IS THE END",
+				"QAQAQ"
+			)), true).symbolsCount(),
+			42L
+		);
+
+		Map.of(false, 35, true, 29).forEach((diagonal, expectedResult) -> expect(slopeCalculator("""
+			..........
+			..###.##..
+			...####...
+			..######..
+			..######..
+			...####...
+			..........""", diagonal), expectedResult));
+	}
+
+	private record RunicWordsAndSymbolsCount(int wordCount, long symbolsCount) {}
+}
--- src/main/java/fi/kissakala/Utils.java
+++ src/main/java/fi/kissakala/Utils.java
@@ -1,11 +1,16 @@
+package fi.kissakala;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,6 +30,16 @@ public class Utils {
 		return readInput(filename).toCharArray();
 	}
 
+	@SuppressWarnings("unchecked")
+	public static <T> T[][] stringAs2DArray(final String string, final Function<String, T> mapper, final Class<T> clazz) {
+		return Arrays.stream(LINE_BREAK_PATTERN.split(string))
+			.map(row -> Arrays.stream(row.split(""))
+				.map(mapper)
+				.toArray(len -> (T[]) Array.newInstance(clazz, len))
+			)
+			.toArray(size -> (T[][]) Array.newInstance(clazz, size, 0));
+	}
+
 	public static Matcher matchInput(final String input, final Pattern pattern) {
 		final Matcher matcher = pattern.matcher(input);
 		if (matcher.matches()) {
@@ -35,6 +50,23 @@ public class Utils {
 
 	public static String reverse(final String string) {
 		return new StringBuffer(string).reverse().toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T[][] pad2DArray(final T[][] array, final int padding, final T paddingValue) {
+		final T[][] result = (T[][]) Array.newInstance(
+			array.getClass().getComponentType().getComponentType(),
+			array.length + (padding * 2), array[0].length + (padding * 2)
+		);
+
+		for (T[] row : result) {
+			Arrays.fill(row, paddingValue);
+		}
+		for (int y = 0; y < array.length; y++) {
+			System.arraycopy(array[y], 0, result[y + padding], padding, array[y].length);
+		}
+
+		return result;
 	}
 
 	public static void run(final String task, final Callable<Object> callable) throws Exception {
@@ -48,4 +80,6 @@ public class Utils {
 			throw new AssertionError("Expected %s, got %s".formatted(toBe, o));
 		}
 	}
+
+	public record XY(int x, int y) {}
 }
